083ff918194f123234915f3ef8c4ba13b4d3bad0
Daidha <dai.chavez88@gmail.com>
2023-12-04 10:52:59 +0100

    Primer commit con las variables de Python

---
+++ 01_variables.py
@@ -0,0 +1,10 @@
+#crear una variable en Pythnon
+nombre = "Daidha" #tipo String
+numero1 = 10
+numero2 = 10.5
+booleano1 = True
+booleano2 = False
+
+#print es una función que muestra información por el terminal
+print(nombre)
+print(type(nombre)) #se ejecuta primero la que esta mas adentro del parentesis
