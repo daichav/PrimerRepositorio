172d9114fd723382d271a1f54e2c3b35d0721f39
Daidha <dai.chavez88@gmail.com>
2023-12-04 11:43:07 +0100

    Añadido fichero 02_operadores.py

---
+++ 02_operadores.py
@@ -0,0 +1,18 @@
+# Operadores
+
+num1 = 11
+num2 = 2
+
+print("La suma es",num1 + num2)
+print("La resta es",num1 - num2)
+print("La multiplicación es",num1 * num2)
+print("La división es",num1 / num2)
+
+
+#11/2 = 5 y "sobra 1"--> ese 1 se llama modulo y tiene este simbolo %
+#modulo 11%2 -->1 
+
+print("El módulo es",num1 % num2)
+print("El exponente es",num1 ** num2)
+
+
